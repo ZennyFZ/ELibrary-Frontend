43c094c1efab5d89e5aa17d05d9d2a5cc4ebe784
Zenny <nhandinhdo291@gmail.com>
2024-01-09 18:46:11 +0700

    add production enviroment detector

--- src/apis/APIConfig.js
+++ src/apis/APIConfig.js
@@ -1,4 +1,7 @@
-const host = 'localhost:8000/api/v1';
+let host = 'localhost:8000/api/v1';
+if(import.meta.env.ENVIROMENT === 'production'){
+  host = 'e-library-backend-pi.vercel.app/api/v1';
+}
 export const USER_API_URL = `http://${host}/users`;
 export const BOOK_API_URL = `http://${host}/book`;
 export const TRANSLATION_API_URL = `http://${host}/translate`;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dotenv from 'dotenv'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.ENVIROMENT': JSON.stringify(process.env.ENVIROMENT),
+  }
 })
