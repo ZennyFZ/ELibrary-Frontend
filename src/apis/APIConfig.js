859ce7d53d5171e8282274c681c871f81f17aa29
Zenny <nhandinhdo291@gmail.com>
2024-01-09 19:07:07 +0700

    quick fix for env

--- src/apis/APIConfig.js
+++ src/apis/APIConfig.js
@@ -1,7 +1,7 @@
-let host = 'localhost:8000/api/v1';
-if(import.meta.env.ENVIROMENT === 'production'){
-  host = 'e-library-backend-pi.vercel.app/api/v1';
-}
+let host = '';
+console.log(import.meta.env.VITE_ENVIROMENT);
+import.meta.env.VITE_ENVIROMENT == 'production' ? host = 'e-library-backend-pi.vercel.app/api/v1' : host = 'localhost:8000/api/v1';
+
 export const USER_API_URL = `http://${host}/users`;
 export const BOOK_API_URL = `http://${host}/book`;
 export const TRANSLATION_API_URL = `http://${host}/translate`;
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import dotenv from 'dotenv'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env.ENVIROMENT': JSON.stringify(process.env.ENVIROMENT),
+    'process.env.VITE_ENVIROMENT': JSON.stringify(process.env.VITE_ENVIROMENT),
   }
 })
