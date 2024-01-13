1c3d865e84267f8ea52506a057a35feb88debe5e
Zenny <nhandinhdo291@gmail.com>
2024-01-13 08:21:14 +0700

    add PaymentService

--- src/apis/APIConfig.js
+++ src/apis/APIConfig.js
@@ -4,5 +4,6 @@ import.meta.env.VITE_ENVIROMENT == 'production' ? host = 'https://e-library-back
 export const USER_API_URL = `${host}/user`;
 export const BOOK_API_URL = `${host}/book`;
 export const CATEGORY_API_URL = `${host}/category`;
+export const PAYMENT_API_URL = `${host}/payment`;
 export const TRANSLATION_API_URL = `${host}/translate`;
 export const SUMMARIZATION_API_URL = `${host}/summarize`;
