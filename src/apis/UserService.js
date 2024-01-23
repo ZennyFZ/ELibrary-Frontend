41e00b79bbef32ebd25f8d1bb25d518af0f16720
Zenny <nhandinhdo291@gmail.com>
2024-01-23 21:06:42 +0700

    fix api and add chatbot

--- src/apis/UserService.js
+++ src/apis/UserService.js
@@ -17,6 +17,10 @@ const getCurrentUser = () => {
     return axios.get(`${USER_API_URL}/get-current-user`,{ withCredentials: true });
 }
 
+const getBooks = (userId) => {
+    return axios.get(`${USER_API_URL}/get-books/${userId}`, { withCredentials: true });
+}
+
 const updateProfile = (name, email, dob, phone) => {
     return axios.put(`${USER_API_URL}/update-profile`, { name, email, dob, phone }, { withCredentials: true });
 }
@@ -29,4 +33,4 @@ const updateUserRole = (id, role) => {
     return axios.post(`${USER_API_URL}/update-user-role`, { id, role }, { withCredentials: true });
 }
 
-export { login, register, logout, getCurrentUser, updateProfile, changePassword, updateUserRole }
+export { login, register, logout, getCurrentUser, getBooks, updateProfile, changePassword, updateUserRole }
